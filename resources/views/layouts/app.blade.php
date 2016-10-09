c1f47f4f4db2aa2d5142ef716b8090a7240b7c23
Erik <erik.c.forsberg@gmail.com>
2016-10-09 17:03:50 +0200

    Svenska auth-länktexter

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -92,8 +92,8 @@
             <ul class="nav navbar-nav navbar-right">
                 <!-- Authentication Links -->
                 @if (Auth::guest())
-                    <li><a href="{{ url('/login') }}">Login</a></li>
-                    <li><a href="{{ url('/register') }}">Register</a></li>
+                    <li><a href="{{ url('/login') }}">Logga in</a></li>
+                    <li><a href="{{ url('/register') }}">Registrera</a></li>
                 @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -105,7 +105,7 @@
                                 <a href="{{ url('/logout') }}"
                                    onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();">
-                                    Logout
+                                    Logga ut
                                 </a>
 
                                 <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
