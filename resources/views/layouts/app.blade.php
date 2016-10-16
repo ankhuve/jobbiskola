5b8338f6007d15af7d19363081eb7851c96432e9
Erik <erik.c.forsberg@gmail.com>
2016-10-16 19:27:34 +0200

    og-image

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
 
     <meta property="og:title" content="@yield('og-title', 'Jobbiskola.se | Lediga jobb inom skola')" />
     <meta property="og:description" content="@yield('og-description', 'Här kan du söka bland tusentals jobb! Oavsett om du är nyutexaminerad eller helt enkelt vill vidare i karriären kan vi hjälpa dig att hitta rätt. Vi jobbar rikstäckande och hjälper allt i från enskilda firmor till stora koncerner, kommuner och myndigheter med att hitta rätt personal. ')" />
-    <meta property="og:image" content="@yield('og-image', asset('img/jobbiskola_large.png'))" />
+    <meta property="og:image" content="@yield('og-image', asset('img/jobbiskola_og.png'))" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
